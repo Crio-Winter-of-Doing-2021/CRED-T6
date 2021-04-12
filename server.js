c41ebaf909ea3601a3502d77da9fc8af4729ef01
Shivam0504 <shivamkirti71@gmail.com>
2021-04-12 19:25:29 +0530

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
 
 connectDB();
 
 app.use(express.json({extended : false}));
 
-
-app.get('/', (req,res)=>res.send('API Running'));
 
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
@@ -17,6 +15,12 @@ app.use('/rewards', require('./routes/rewards'));
 app.use('/transactions', require('./routes/transactions'));
 app.use('/coupons', require('./routes/coupons'));
 
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static('client.build'));
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
